badd0935ef874b1b1509615b83e162e261882b96
thiago miziara <thiagonmiziara@gmail.com>
2021-03-28 16:39:05 -0300

    add home styles | e finalizando a page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,29 @@
 import Head from "next/head";
 
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello Word!</h1>
+
+      <main className={styles.contentConteiner}>
+        <section className={styles.hero}>
+          <span>👏 Hey,welcome</span>
+          <h1>
+            News about
+            <br /> the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
