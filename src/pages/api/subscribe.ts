987844e2e55dbba02705b573a9a188d6b76a01df
thiago miziara <thiagonmiziara@gmail.com>
2021-04-01 20:02:05 -0300

    criando a logica no subscribe.ts para nao criar repetidos custumers no stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,33 +1,60 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import {getSession} from "next-auth/client";
-import {stripe} from '../../service/stripe';
+import { query as q } from "faunadb";
+import { getSession } from "next-auth/client";
+import { stripe } from "../../service/stripe";
+import { fauna } from "../../service/fauna";
+
+type User = {
+  ref: {
+    id: string;
+  };
+
+  data: {
+    stripe_customer_id: string;
+  };
+};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
+    const session = await getSession({ req });
 
-    const session = await getSession({req});
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    );
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
-     // metadata
-    })
+    let custumerId = user.data.stripe_customer_id;
 
-      const stripeCheckoutSession = await stripe.checkout.sessions.create({
-        customer: stripeCustomer.id,        
-        payment_method_types:['card'],
-        billing_address_collection:'required',
-        line_items: [
-          { price:'price_1IaTw7HuvlvAUJetUtypmWDQ', quantity: 1 }
-        ],
-        mode: 'subscription',
-        allow_promotion_codes: true,
-        success_url: process.env.STRIPE_SUCCESS_URL,
-        cancel_url: process.env.STRIPE_CANCEL_URL
-      })
+    if (!custumerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+        // metadata
+      });
 
-      return res.status(200).json({sessionId: stripeCheckoutSession.id})
+      await fauna.query(
+        q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+          data: {
+            stripe_customer_id: stripeCustomer.id,
+          },
+        })
+      );
+
+      custumerId = stripeCustomer.id;
+    }
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: custumerId,
+      payment_method_types: ["card"],
+      billing_address_collection: "required",
+      line_items: [{ price: "price_1IaTw7HuvlvAUJetUtypmWDQ", quantity: 1 }],
+      mode: "subscription",
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    });
+
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id });
   } else {
     res.setHeader("Allow", "POST");
-    res.status(405).end('Method not allowed');
+    res.status(405).end("Method not allowed");
   }
 };
