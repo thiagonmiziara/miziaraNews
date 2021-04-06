61a1ff786cba08a543ddb96cf392f1f6ce00aa3f
thiago miziara <thiagonmiziara@gmail.com>
2021-04-06 19:39:22 -0300

    add conponent Link do Next para fazer o efeito SPA do react

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,25 @@
+import Link from "next/link";
+
 import { SigninButton } from "../SigninButton";
+
 import styles from "./styles.module.scss";
 
-export function Header (){
-    return(
-        <header className={styles.headerContainer}>
-            <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig.news"/>
-                <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
-                </nav>
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts" prefetch>
+            <a>Posts</a>
+          </Link>
+        </nav>
 
-                <SigninButton/>
-            </div>
-        </header>
-
-    );
+        <SigninButton />
+      </div>
+    </header>
+  );
 }
