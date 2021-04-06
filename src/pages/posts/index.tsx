cc236a7587f630cb84cf3b1fa203c00a3e2342b0
thiago miziara <thiagonmiziara@gmail.com>
2021-04-06 19:27:51 -0300

    configurando a requisição a Api do prismic e formatando os dados retornado da API

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,25 @@
 import Head from "next/head";
+
+import { GetStaticProps } from "next";
+import { getPrismicClient } from "../../service/prismic";
+
+import Prismic from "@prismicio/client";
+import {RichText} from "prismic-dom";
+
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string; 
+}
+
+interface PostProps{
+    posts: Post[];
+}
+
+export default function Posts({posts}: PostProps) {
   return (
     <>
       <Head>
@@ -10,34 +28,48 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>15 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerpa & Yarn Workspaces</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet qui
-              voluptate molestias.
-            </p>
-          </a>
-
-          <a href="#">
-            <time>25 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerpa & Yarn Workspaces</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet qui
-              voluptate molestias.
-            </p>
-          </a>
-
-          <a href="#">
-            <time>31 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerpa & Yarn Workspaces</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet qui
-              voluptate molestias.
-            </p>
-          </a>
+            {posts.map(post => (
+              <a href="#" key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p> {post.excerpt}</p>
+              </a>                    
+           ))}                
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "publication")],
+    {
+      fetch: ["post.title", "post.content"],
+      pageSize: 100,
+    }
+  );
+  //console.log('console log aki',JSON.stringify(response, null ,2));
+
+  const posts = response.results.map(post => {
+      return{
+          slug: post.uid,
+          title: RichText.asText(post.data.title),
+          excerpt: post.data.content.find(content => content.type ==="paragraph")?.text ?? "",
+         updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR',{
+             day:"2-digit",
+             month:"long",
+             year: "numeric"
+         })
+      };
+  })
+
+
+  return {
+    props: {
+        posts
+    },
+  };
+};
