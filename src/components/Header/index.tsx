ee3936752fdd63edec4a7bd1c870db7e2010aabf
thiago miziara <thiagonmiziara@gmail.com>
2021-04-06 19:57:07 -0300

    Criando o component activeLink e aprendendo sobre o cloneElement do Reaact

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,23 @@
-import Link from "next/link";
-
 import { SigninButton } from "../SigninButton";
+import { ActiveLink } from "../ActiveLink";
 
 import styles from "./styles.module.scss";
 
 export function Header() {
+
+  
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <Link href="/">
-            <a className={styles.active}>Home</a>
-          </Link>
-          <Link href="/posts" prefetch>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
 
         <SigninButton />
