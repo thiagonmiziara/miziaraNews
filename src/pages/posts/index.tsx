1f3159cd417a5c5dfe23a913c74a658c434eda67
thiago miziara <thiagonmiziara@gmail.com>
2021-04-07 20:19:10 -0300

    add pagina dinamica post e seus estilos

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,25 +1,25 @@
 import Head from "next/head";
-
+import Link from "next/link";
 import { GetStaticProps } from "next";
-import { getPrismicClient } from "../../service/prismic";
 
+import { RichText } from "prismic-dom";
 import Prismic from "@prismicio/client";
-import {RichText} from "prismic-dom";
+import { getPrismicClient } from "../../service/prismic";
 
 import styles from "./styles.module.scss";
 
 type Post = {
-    slug: string;
-    title: string;
-    excerpt: string;
-    updatedAt: string; 
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostProps {
+  posts: Post[];
 }
 
-interface PostProps{
-    posts: Post[];
-}
-
-export default function Posts({posts}: PostProps) {
+export default function Posts({ posts }: PostProps) {
   return (
     <>
       <Head>
@@ -28,13 +28,15 @@ export default function Posts({posts}: PostProps) {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-            {posts.map(post => (
-              <a href="#" key={post.slug}>
+          {posts.map((post) => (
+            <Link href={`/posts/${post.slug}`} key={post.slug}>
+              <a>
                 <time>{post.updatedAt}</time>
                 <strong>{post.title}</strong>
                 <p> {post.excerpt}</p>
-              </a>                    
-           ))}                
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </>
@@ -53,23 +55,27 @@ export const getStaticProps: GetStaticProps = async () => {
   );
   //console.log('console log aki',JSON.stringify(response, null ,2));
 
-  const posts = response.results.map(post => {
-      return{
-          slug: post.uid,
-          title: RichText.asText(post.data.title),
-          excerpt: post.data.content.find(content => content.type ==="paragraph")?.text ?? "",
-         updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR',{
-             day:"2-digit",
-             month:"long",
-             year: "numeric"
-         })
-      };
-  })
-
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
     props: {
-        posts
+      posts,
     },
   };
 };
