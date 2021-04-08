029b1e6f2c736a94cfc8f4a7739c8d32e45efd68
thiago miziara <thiagonmiziara@gmail.com>
2021-04-08 18:33:40 -0300

    add verificação se o usuário tem uma conta active para não deixar ele assinar duas vezes o mesmo plano do blog

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 
+
 import { fauna } from "../../../service/fauna";
 
 export default NextAuth({
@@ -14,6 +15,40 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("user_by_email"),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        };
+
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      };
+    },
+
     async signIn(user, account, profile) {
       const { email } = user;
 
@@ -30,8 +65,7 @@ export default NextAuth({
           )
         );
         return true;
-      } catch(error) {
-
+      } catch (error) {
         return console.log(error), false;
       }
     },
