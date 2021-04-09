3463d573bed1d5fd5dc1e1426a7b786451ed32a1
thiago miziara <thiagonmiziara@gmail.com>
2021-04-09 17:27:50 -0300

    Coreção de bugs

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -18,7 +18,7 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
       return;
     }
 
-    if(session.activeSubscription){
+    if(session?.activeSubscription){
       router.push('/posts');
       return;
     }
