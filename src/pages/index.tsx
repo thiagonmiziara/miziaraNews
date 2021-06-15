3ce673a83a83e6793f01590ba75628ebb3c575a7
thiago miziara <thiagonmiziara@gmail.com>
2021-06-15 18:37:01 -0300

    testando page Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ import styles from "./home.module.scss";
 interface HomeProps {
   product: {
     priceId: string;
-    amount: number;
+    amount: string;
   };
 }
 
@@ -32,7 +32,7 @@ export default function Home({ product }: HomeProps) {
             <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
